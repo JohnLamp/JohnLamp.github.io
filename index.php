7e0429d261973dfc00feeaaa0de0164d8384ace5
John <johnlamp7@gmail.com>
2025-05-22 23:39:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="portfoliocss.css">
     <title>Document</title>
-</head>
+<head>
 <body>
     <header>
         <h1>John<br>Lampouras</h1><br>
